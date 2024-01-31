5e58ce3303b9817d47cb0a8cff086da972812bef
root <nourisoukaina11@gmail.com>
2024-01-31 06:22:29 -0800

    all

---
+++ 0x00-pagination/main/0-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""_summary_"""
+
+index_range = __import__('../0-simple_helper_function').index_range
+
+res = index_range(1, 7)
+print(type(res))
+print(res)
+
+res = index_range(page=3, page_size=15)
+print(type(res))
+print(res)
